17e22b2d3c42c453b0f07df9397008353bd7285c
Tiago Gomes <tiagog@futurefuel.io>
2018-06-23 23:07:13 +0100

    refactoring folder sturcture

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { fetchCityWeather } from './api'
+import { fetchCityWeather } from '../utils/api'
 
 class App extends Component {
   componentDidMount() {
@@ -12,7 +10,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
