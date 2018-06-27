8b6f40db0f62379792348336fe948e9b220d95fd
Tiago Gomes <tiagog@futurefuel.io>
2018-06-27 03:53:33 +0100

    change api arguments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const API_KEY = '1fe7e92382cd382aacdf16eb9f107701'
-export const API_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+export const API_URL = `http://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${API_KEY}`
